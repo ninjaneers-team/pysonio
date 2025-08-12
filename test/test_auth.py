7ec77ccceac63d3da757330a4f39044c6f0230d9
Michael Gerhold <michael.gerhold@ninjaneers.de>
2025-08-12 12:23:25 +0200

    feat: move common test code into `conftest.py`

--- test/test_auth.py
+++ test/test_auth.py
@@ -1,40 +1,13 @@
-import os
 from datetime import datetime
 from datetime import timedelta
 from typing import Final
 
 import pytest
-from dotenv import load_dotenv
 
 from pysonio import AuthenticationError
 from pysonio import AuthToken
 from pysonio import Client
 from pysonio.models.authentication import OAuth2ErrorType
-
-load_dotenv()
-
-
-def get_environment_variable_or_raise(key: str) -> str:
-    value: Final = os.getenv(key)
-    if value is None:
-        raise ValueError(f"Environment variable '{key}' is not set.")
-    return value
-
-
-CLIENT_ID = get_environment_variable_or_raise("CLIENT_ID")
-CLIENT_SECRET = get_environment_variable_or_raise("CLIENT_SECRET")
-PARTNER_ID = get_environment_variable_or_raise("PARTNER_ID")
-APP_ID = get_environment_variable_or_raise("APP_ID")
-
-
-@pytest.fixture
-def client() -> Client:
-    return Client(
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        personio_partner_identifier=PARTNER_ID,
-        personio_app_identifier=APP_ID,
-    )
 
 
 def test_invalid_credentials_raises_authentication_error() -> None:
